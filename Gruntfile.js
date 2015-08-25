41d96868132be0a846a91a2b031fc5fffc3158d2
Hirse <jan.pilzer@gmx.de>
2015-08-25 21:08:11 +0200

    Add grunt compress task

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        compress: {
+            main: {
+                options: {
+                    archive: 'brackets-print-margin.zip'
+                },
+                files: [
+                    {
+                        src: [
+                            'nls/**',
+                            'styles/*',
+                            'templates/*',
+                            'CHANGELOG.md',
+                            'LICENSE',
+                            'main.js',
+                            'package.json',
+                            'README.md',
+                            'strings.js'
+                        ]
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compress');
+
+    grunt.registerTask('default', ['compress']);
+};
